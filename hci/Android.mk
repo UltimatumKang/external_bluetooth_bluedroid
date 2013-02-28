eba007b7fd2450ed87bb1ed7a201529626fefef2
Pradeep Panigrahi <pradeepp@codeaurora.org>
2013-02-28 19:22:19 +0530

    Bluetooth: Generalize bluedroid for smd and uart based soc's.
    Remove compile time dependency to use bluedroid for SMD
    based soc or uart based soc.
    
    Change-Id: I1a6c3faf8c236056e9860c1af7a7db378a2a8f66
    
    Conflicts:
    
    	hci/Android.mk

--- hci/Android.mk
+++ hci/Android.mk
@@ -11,16 +11,6 @@ LOCAL_SRC_FILES := \
         src/btsnoop.c \
         src/utils.c
 
-ifeq ($(BLUETOOTH_HCI_USE_MCT),true)
-
-LOCAL_CFLAGS += -DHCI_USE_MCT
-
-ifeq ($(QCOM_BT_USE_SMD_TTY),true)
-
-LOCAL_CFLAGS += -DQCOM_WCN_SSR
-
-endif
-
 ifeq ($(QCOM_BT_USE_SMD_TTY),true)
 
 LOCAL_CFLAGS += -DQCOM_WCN_SSR
@@ -28,16 +18,10 @@ LOCAL_CFLAGS += -DQCOM_WCN_SSR
 endif
 
 LOCAL_SRC_FILES += \
+        src/userial.c \
+        src/userial_mct.c \
         src/hci_mct.c \
-        src/userial_mct.c
-
-else
-
-LOCAL_SRC_FILES += \
-        src/hci_h4.c \
-        src/userial.c
-
-endif
+        src/hci_h4.c
 
 LOCAL_C_INCLUDES += \
         $(LOCAL_PATH)/include \
